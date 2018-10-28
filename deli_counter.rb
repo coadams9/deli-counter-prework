2854a35289f1386f9bc130a2a8df9a6a7539792e
Cory Adams <coadams9@gmail.com>
2018-10-28 02:11:53 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,9 @@
-# Write your code here.
+katz_deli = []
+
+def line
+  if katz_deli.empty?
+    puts "The line is currently empty."
+  elsif katz_deli << array[index] 
+    puts katz_deli
+  end
+end
