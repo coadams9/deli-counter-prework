80790f79a9ecc5adc253264da4ed67d2fe1a1771
Cory Adams <coadams9@gmail.com>
2018-10-28 02:46:48 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,9 +5,10 @@ def line(katz_deli)
     puts "The line is currently empty."
   else
     newline = "The line is currently:"
-    katz_deli.each.with_index do |person, i|
-    newline << #{i + 1}. {person}"
-    end
+      katz_deli.each.with_index do |person, i|
+      newline << #{i + 1}. {person}"
+      end
   end
+  
   puts newline
 end
