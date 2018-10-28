79c57c8a50dca5ffccd0456f6c0fbf1c8c16f80e
Cory Adams <coadams9@gmail.com>
2018-10-28 02:39:58 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(katz_deli)
   if katz_deli.empty?
     puts "The line is currently empty."
   else
-    katz_deli.each_with_index do |person, i|
+    katz_deli.each_with_index(1) do |person, i|
       puts "The line is currently: #{i}. {person}"
     end
   end
